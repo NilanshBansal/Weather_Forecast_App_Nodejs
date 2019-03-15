ee84ec1a8e69d6ba7585b5ddc31d4295ab71825e
Nilansh Bansal <bansalnilansh@gmail.com>
2019-03-15 15:03:35 +0530

    modifications

--- routes/routes.js
+++ routes/routes.js
@@ -10,10 +10,6 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Weather Forecast Application' });
 });
 
-router.get('/test',function(req,res,next){
-  res.render('test',{title:'Weather Forecast Application '});
-});
-
 router.get('/graph', function (req, res, next) {
   if(!req.query.hasOwnProperty('place')){
     res.status(400);
@@ -40,62 +36,6 @@ router.get('/graph', function (req, res, next) {
       }
       return res.render('graph',{labels: date,temp,pressure,humidity});
 
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.status(400);
-      return res.send(error);
-    });
-});
-
-router.get('/weather', function (req, res, next) {
-  if(!req.query.hasOwnProperty('place')){
-    res.status(400);
-    return res.send("Please specify query parameter place");
-  }
-  axios.get(url+ req.query.place + app_id)
-    .then(function (response) {
-      temp = null
-      pressure = null
-      humidity = null
-      if (response.hasOwnProperty('data') && response.data.hasOwnProperty('main')){
-        temp = response.data.main.temp;
-        pressure = response.data.main.pressure;
-        humidity = response.data.main.humidity;
-      }
-      return res.render('weather', { temp,pressure,humidity});
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.status(400);
-      return res.send(error);
-    });
-});
-
-
-router.get('/weather_forecast', function (req, res, next) {
-  if(!req.query.hasOwnProperty('place')){
-    res.status(400);
-    return res.send("Please specify query parameter place");
-  }
-  axios.get(forecast_url+ req.query.place + app_id)
-    .then(function (response) {
-      items = []
-      output = []
-      if (response.hasOwnProperty('data') && response.data.hasOwnProperty('list')){
-        items = response.data.list;
-      }
-      for(var i=0;i< items.length;i++){
-        time = items[i].dt_txt.substring(10);
-        if(time.indexOf("12:00:00") > -1){
-          temp = items[i].main.temp;
-          pressure = items[i].main.pressure;
-          humidity = items[i].main.humidity;
-          date = items[i].dt_txt.split(" ")[0];
-          output.push({temp,pressure,humidity,date});
-        }
-      }
-      return res.render('forecast', {'forecast':output,'place':req.query.place});
     })
     .catch(function (error) {
       console.log(error);
@@ -135,7 +75,7 @@ router.get('/ajax_weather_forecast', function (req, res, next) {
     });
 });
 
-router.get('/ajax_weather', function (req, res, next) {
+router.get('/ajax_weather_today', function (req, res, next) {
   if(!req.query.hasOwnProperty('place')){
     res.status(400);
     return res.send("Please specify query parameter place");
